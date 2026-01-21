e4d28480c5e83678c402aad0b107cbff73d0ff41
Hpayeur <hailpaye017@gmail.com>
2026-01-21 14:57:26 -0700

    Nearly Done

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let todoArray = [];
 const text = document.getElementById("text");
 const addTaskButton = document.getElementById("add-task-btn");
 const saveTaskButton = document.getElementById("save-todo-btn");
+const listBox = document.getElementById("listBox");
+const saveInd = document.getElementById("saveIndex");
 const itemInput = document.getElementById("itemInput");
 const itemList = document.getElementById("itemList");
 
@@ -41,59 +43,55 @@ text.value = "";
 localStorage.setItem("todo", JSON.stringify(todoArray));
 displayTodo();
 
-function displayTodos(todoList) {}
+function displayTodo() {
+  let todo = localStorage.getItem("todo");
+  if (todo === null) {
+    todoArray = [];
+  } else {
+    todoArray = JSON.parse(todo);
+  }
+  let htmlCode = "";
+  todoArray.forEach((list, ind) => {
+    htmlCode += `<div class='flex mb-4 items-center'>
+          <p class='w-full text-white font-bold'>${list}</p>
+          <button onclick='edit(${ind})' class='flex-no-shrink p-2 ml-4 mr-2 rounded text-white text-grey bg-green-600'>Edit</button>
+          <button onclick='deleteTodo(${ind})' class='flex-no-shrink p-2 ml-2 rounded text-white bg-red-500'>Delete</button>
+       </div>`;
+  });
+  listBox.innerHTML = htmlCode;
+}
 
-// Add code below this comment to do the following:
-// 1. when the page loads, call displayTodo() method
+function deleteTodo(ind) {
+  let todo = localStorage.getItem("todo");
+  if (todo === null) {
+    todoArray = [];
+  } else {
+    todoArray = JSON.parse(todo);
+  }
+  todoArray.splice(ind, 1);
+  localStorage.setItem("todo", JSON.stringify(todoArray));
+  displayTodo();
+}
 
-// This method is already in place for you.
+function edit(ind) {
+  saveInd.value = ind;
+  let todo = localStorage.getItem("todo");
+  let todoArray = JSON.parse(todo);
+  text.value = todoArray[ind];
+  document.getElementById("add-task-btn").style.display = "none";
+  document.getElementById("save-todo-btn").style.display = "block";
+}
 
-// function displayTodo() {
-//   let todo = localStorage.getItem("todo");
-//   if (todo === null) {
-//     todoArray = [];
-//   } else {
-//     todoArray = JSON.parse(todo);
-//   }
-//   let htmlCode = "";
-//   todoArray.forEach((list, ind) => {
-//     htmlCode += `<div class='flex mb-4 items-center'>
-//           <p class='w-full text-white font-bold'>${list}</p>
-//           <button onclick='edit(${ind})' class='flex-no-shrink p-2 ml-4 mr-2 rounded text-white text-grey bg-green-600'>Edit</button>
-//           <button onclick='deleteTodo(${ind})' class='flex-no-shrink p-2 ml-2 rounded text-white bg-red-500'>Delete</button>
-//        </div>`;
-//   });
-//   listBox.innerHTML = htmlCode;
-// }
-
-// function deleteTodo(ind) {}
-
-// call the todo and let it equal localstorage.getitem("todo")
-// assign the todoArray equal to JSON.parse(todo)
-// use the todoArray and use the splice method on the ind and pass in 1 as well.
-// set the todo in local storage and use the JSON.stringify(todoArray)
-// call the display todo method
-
-// function edit(ind) {}
-// set the saveInd.value equal to the ind
-// call the todo and let it equal localstorage.getitem("todo")
-// assign the todoArray equal to JSON.parse(todo)
-// assign the text.value to the array and get the index [ind].
-// set the addTaskButton display to none
-// set the saveTaskButton display to block
-
-// saveTaskButton.addEventListener("click", () => {});
-
-// this is the challenge for this project
-// in this part you will need to add the following:
-// 1. call the todo and let it equal localstorage.getitem("todo")
-// 2. assign the todoArray equal to JSON.parse(todo)
-// then finish out the rest of the following instructions:
-// 1. let id be the same as your saveInd.value
-// 2. switch the add and save displays to block and none respectively.
-// 3. set text value to empty
-// 4. and use the localstorage method setItem, pass in todo and stringify the array.
-// 5. display todo method called
-
-// const listBox = document.getElementById("listBox");
-// const saveInd = document.getElementById("saveIndex");
+saveTaskButton.addEventListener("click", () => {
+  let todos = localStorage.getItem("todo");
+  let todoArray = JSON.parse(todos) || [];
+  let id = parseInt(saveInd.value);
+  if (id >= 0 && id < todoArray.length) {
+    todoArray[id].task = todoInput.value;
+    addDisplay.style.display = "block";
+    saveDisplay.style.display = "none";
+    todoInput.value = "";
+    localStorage.setItem("todo", JSON.stringify(todoArray));
+    displayTodo();
+  }
+});
